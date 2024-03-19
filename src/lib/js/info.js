cf505cddb4efa741c098e85c73d4abcbad4bdbe3
Koen Raijer <koenrayer@gmail.com>
2024-03-19 14:09:31 +0100

    fixed errors for projects page

--- src/lib/js/info.js
+++ src/lib/js/info.js
@@ -23,6 +23,19 @@ export const email = 'koenrayer@gmail.com'
 export const avatar = 'avatar.webp'
 
 export const bio = { 
-    text: `Hi, I'm Koen. I'm a medical doctor doing research on the crossroads of psychiatry and data science. I'm based in Sittard, the Netherlands. My research concerns predicting when people are having intrusions from past traumatic events using physiological data and machine learning. This is where I share my thoughts on a non-specific set of topics, although admittedly, it's mostly tacky programming tutorials because I can't stop myself from tweaking the website rather than writing posts.`,
-    html: `Hey there, I'm Koen — a medical doctor at the intersection of psychiatry and data science, based in <s>Maastricht</s> Sittard, the Netherlands. My research concerns the detection of intrusive memories using physiological data and machine learning. This is where I <i>would like</i> to share my thoughts on a non-specific set of topics, although, admittedly, most are short programming tidbits because I can't stop myself from tweaking the website rather than writing posts. `
+    text: `👋 Hey there, I'm Koen - a medical doctor doing research on the crossroads of psychiatry and data science. I'm based in Sittard, the Netherlands. My research concerns predicting when people are having intrusions from past traumatic events using physiological data and machine learning. This is where I share my thoughts on a non-specific set of topics, although admittedly, it's mostly tacky programming tutorials because I can't stop myself from tweaking the website rather than writing posts.`,
+    html: `👋 Hey there, I'm Koen — a medical doctor at the intersection of psychiatry and data science, based in <s>Maastricht</s> Sittard, the Netherlands. My research concerns the detection of intrusive memories using physiological data and machine learning. This is where I <i>would like</i> to share my thoughts on a non-specific set of topics, although, admittedly, most are short programming tidbits because I can't stop myself from tweaking the website rather than writing posts. `
+}
+
+export const projects = {
+    "ReAsset": {
+        "title": "ReAsset",
+        "time_period" : "2023",
+        "description": "A web app to help you rebalance assets your investment portfolio with the minimum amount of transactions, saving you time and money.",
+        "image": "project_reasset.png",
+        "link": "https://reasset.vercel.app/", 
+        "code" : "https://github.com/koenraijer/reasset",
+        "languages": [ "Javscript", "SvelteKit", "TailwindCSS"],
+        "finished" : false
+    },
 }
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,39 +1,7 @@
 <script>
     import Project from '$lib/components/Project.svelte';
     import PageTitle from '$lib/components/PageTitle.svelte';
-    
-    let projects = {
-        "ReAsset": {
-            "title": "ReAsset",
-            "time_period" : "2023",
-            "description": "A web app to help you rebalance assets your investment portfolio with the minimum amount of transactions, saving you time and money.",
-            "image": "project_reasset.png",
-            "link": "https://www.reasset.vercel.app/", 
-            "code" : "https://github.com/koenraijer/reasset",
-            "languages": [ "Javscript", "SvelteKit", "TailwindCSS"],
-            "finished" : false
-        },
-        "project2": {
-            "title": "Project 2",
-            "time_period" : "2020 - 2021",
-            "description": "This is a description of project 2",
-            "image": "project2.jpg",
-            "link": "https://www.google.com", 
-            "code": "google.com",
-            "languages": ["Python", "JavaScript", "HTML", "CSS", "Svelte"],
-            "finished" : true
-        },
-        "project3": {
-            "title": "Project 3",
-            "time_period" : "2019 - 2020",
-            "description": "This is a description of project 3",
-            "image": "project3.jpg",
-            "link": "https://www.google.com", 
-            "code" : "google.com",
-            "languages": ["Python", "JavaScript", "HTML", "CSS", "Svelte"],
-            "finished" : false
-        }
-    }
+    import { projects } from '$lib/js/info.js';
 </script>
 
 <div class="section">
